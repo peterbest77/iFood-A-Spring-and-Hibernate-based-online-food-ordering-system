f4e749cd825dc41a2f1ed76e49e2c96fe0621e7e
peterbest77 <89502107+peterbest77@users.noreply.github.com>
2022-11-05 21:07:44 -0700

    Update ApplicationConfig.java

--- src/main/java/com/laioffer/onlineOrder/ApplicationConfig.java
+++ src/main/java/com/laioffer/onlineOrder/ApplicationConfig.java
@@ -8,15 +8,10 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@Configuration注释让spring可以托管
-//@EnableWebMvc将config类的类注入IoC
-//哪个IoC？??????????应该是根IoC
 @Configuration
 @EnableWebMvc
 public class ApplicationConfig {
-    //托管方法
-    //为什么要@Beam
-    //sessionFactory将所有实体类对象引入，以后可以开session对实体类进行操作
+
     @Bean(name = "sessionFactory")
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
@@ -25,13 +20,12 @@ public class ApplicationConfig {
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
-    //托管方法
-    //连接数据库
+ 
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-        String RDS_ENDPOINT = "sean-instance.c2nvmjewvtxk.us-east-2.rds.amazonaws.com";
-        String USERNAME = "peterbest7";
-        String PASSWORD = "Peterbest66";
+        String RDS_ENDPOINT = "";
+        String USERNAME = "";
+        String PASSWORD = "";
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://" + RDS_ENDPOINT + ":3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
