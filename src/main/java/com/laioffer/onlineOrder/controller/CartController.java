61ee13fcf07c21670efe3425b432dadf74eca044
peterbest77 <89502107+peterbest77@users.noreply.github.com>
2022-11-05 21:13:36 -0700

    Update CartController.java

--- src/main/java/com/laioffer/onlineOrder/controller/CartController.java
+++ src/main/java/com/laioffer/onlineOrder/controller/CartController.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//@Controller注释的意思是让spring知道这是控制层
+
 @Controller
 public class CartController {
 
-    //注入CartService类用@Autowired
+
     @Autowired
     private CartService cartService;
 
-    //@RequestMapping处理前端/cart的请求
-    //@ResponseBody返回json对象
+ 
     @RequestMapping(value = "/cart", method = RequestMethod.GET)
     @ResponseBody
     public Cart getCart(){
