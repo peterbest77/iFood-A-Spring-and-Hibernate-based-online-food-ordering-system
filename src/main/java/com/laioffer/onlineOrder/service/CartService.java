0f8ff4bce17ba04ac274149817ef8c2588d0b4f4
peterbest77 <89502107+peterbest77@users.noreply.github.com>
2022-11-05 21:10:16 -0700

    Update CartService.java

--- src/main/java/com/laioffer/onlineOrder/service/CartService.java
+++ src/main/java/com/laioffer/onlineOrder/service/CartService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CartService {
     @Autowired
-    private CartDao cartDao; // 其实这个要注入的是 CartDao implements XXX  中的xxx，所以到时候改CartDao，我还是注入的xxx，所以可以减少耦合， 因为这个项目太容易，就直接注入Dao了
+    private CartDao cartDao; 
 
     @Autowired
     private CustomerService customerService;
